06e60fd2b3b0699dcfc0121bc0cf0ac2b09b6f84
tvminh4405 <minhtrinhvan4405@gmail.com>
2025-03-01 16:25:27 +0700

    add

--- Lab01/lab01bai63.java
+++ Lab01/lab01bai63.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class lab01bai63 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhap chieu cao: ");
+        System.out.print("Nhap: n = ");
         int n = scanner.nextInt();
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < 2 * i - 1; j++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
