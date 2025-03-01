bfc04b416592a88f8076cb6e3ab2a3addd7eca11
tvminh4405 <minhtrinhvan4405@gmail.com>
2025-03-01 16:20:59 +0700

    add

--- Lab01/lab01bai63.java
+++ Lab01/lab01bai63.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
-public class lab01bai63{
-    public static void main(String args[]){ 
-        Scanner keyboard = new Scanner(System.in);
-
-        System.out.println("What's your name?");
-        String strName = keyboard.nextLine();
-
-        System.out.println("How old are you?");
-        int iAge = keyboard.nextInt();
-
-        System.out.println("How tall are you (m)?");
-        double dHeight = keyboard.nextDouble();
-
-        System.out.println("Mrs/Ms. " + strName + ", " + iAge + " years old." + " Your height is " + dHeight + ".");
-        keyboard.close();
+public class lab01bai63 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap chieu cao: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
     }
 }
